0c748701c2e1246254dca77f1a5ec82261dd59c9
Ayushi Kolay <ayushi18071998@gmail.com>
2021-11-30 01:30:04 +0530

    add search results

--- src/index.js
+++ src/index.js
@@ -1,11 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import thunk from "redux-thunk";
+import { createStore, applyMiddleware } from "redux";
 import "./index.css";
 import App from "./Components/App";
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer);
+//function logger
+// const logger = function ({ dispatch, getState}) {
+//     return function (next) {
+//         return function (action) {
+//             //middleware code
+//             if(typeof action !== 'function')
+//             {
+//                 console.log(action.type);
+//             }
+//             next(action);
+//         }
+//     }
+// }
+
+const logger = ({dispatch, getState}) => (next) => (action) => {
+    //logger code
+    if(typeof action !== 'function')
+    {
+        console.log(action.type);
+    }
+    next(action);
+}
+
+// const thunk = ({dispatch, getState}) => (next) => (action) => {
+//     //logger code
+//     if(typeof action === 'function')
+//     {
+//         action(dispatch);
+//         return;
+//     }
+//     next(action);
+// }
+
+const store = createStore(rootReducer, applyMiddleware(logger,thunk));
 console.log('store',store);
 // console.log('Before State ',store.getState());
 
